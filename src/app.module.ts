a11cfec99aae9532154c75d75a2a5df445e7e127
fedapon <fedapon@gmail.com>
2022-03-23 21:26:09 -0300

    database config added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SalesModule } from './sales/sales.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true,
+    }),
+
+    SalesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
