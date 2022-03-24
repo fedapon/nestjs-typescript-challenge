6746ee8ba213b27b11f0e54dac7a6ecc3898d03d
fedapon <fedapon@gmail.com>
2022-03-24 00:32:03 -0300

    agent controller and service dummy test

--- src/sales/controllers/agent/agent.controller.ts
+++ src/sales/controllers/agent/agent.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { AgentService } from 'src/sales/services/agent/agent.service';
 
 @Controller('agent')
-export class AgentController {}
+export class AgentController {
+  constructor(private agentService: AgentService) {}
+
+  @Get('/')
+  async get() {
+    return this.agentService.findOneById('A001');
+  }
+}
--- src/sales/services/agent/agent.service.ts
+++ src/sales/services/agent/agent.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Agent } from 'src/sales/models/agent.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class AgentService {}
+export class AgentService {
+  constructor(@InjectRepository(Agent) private agentRepo: Repository<Agent>) {}
+
+  async findOneById(agentCode): Promise<Agent | undefined> {
+    return this.agentRepo.findOne(agentCode);
+  }
+}
