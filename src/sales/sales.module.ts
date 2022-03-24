5b8e49d7527a585b3cce527ed6a32d182dc3a330
fedapon <fedapon@gmail.com>
2022-03-24 00:31:23 -0300

    added import of models

--- src/sales/sales.module.ts
+++ src/sales/sales.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Agent } from 'src/sales/models/agent.entity';
+import { Customer } from 'src/sales/models/customer.entity';
+import { Order } from 'src/sales/models/order.entity';
 import { AgentController } from './controllers/agent/agent.controller';
 import { CustomerController } from './controllers/customer/customer.controller';
 import { OrderController } from './controllers/order/order.controller';
@@ -7,6 +11,7 @@ import { CustomerService } from './services/customer/customer.service';
 import { OrderService } from './services/order/order.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Agent, Customer, Order])],
   controllers: [AgentController, CustomerController, OrderController],
   providers: [AgentService, CustomerService, OrderService],
 })
