1b01148719a634d04e6143eeb7aadf9d327200cc
fedapon <fedapon@gmail.com>
2022-03-25 11:08:52 -0300

    fixed UpdateCustomDto validations

--- src/sales/controllers/customer/dto/update-customer.dto.ts
+++ src/sales/controllers/customer/dto/update-customer.dto.ts
@@ -8,6 +8,7 @@ import {
 import { Agent } from 'src/sales/models/agent.entity';
 
 export class UpdateCustomerDto {
+  @IsOptional()
   @IsNotEmpty()
   @MaxLength(40)
   custName: string;
@@ -16,10 +17,12 @@ export class UpdateCustomerDto {
   @MaxLength(35)
   custCity: string;
 
+  @IsOptional()
   @IsNotEmpty()
   @MaxLength(35)
   workingArea: string;
 
+  @IsOptional()
   @IsNotEmpty()
   @MaxLength(20)
   custCountry: string;
@@ -28,26 +31,32 @@ export class UpdateCustomerDto {
   @IsInt()
   grade: number;
 
+  @IsOptional()
   @IsNotEmpty()
   @IsDecimal()
   openingAmt: number;
 
+  @IsOptional()
   @IsNotEmpty()
   @IsDecimal()
   receiveAmt: number;
 
+  @IsOptional()
   @IsNotEmpty()
   @IsDecimal()
   paymentAmt: number;
 
+  @IsOptional()
   @IsNotEmpty()
   @IsDecimal()
   outstandingAmt: number;
 
+  @IsOptional()
   @IsNotEmpty()
   @MaxLength(15)
   phoneNo: string;
 
+  @IsOptional()
   @IsNotEmpty()
   @MaxLength(6)
   agentCode: Agent;
