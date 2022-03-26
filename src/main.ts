079539f954a98fb46014e9602a17a2804dc71794
fedapon <fedapon@gmail.com>
2022-03-26 16:57:14 -0300

    swagger installed

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -11,6 +12,15 @@ async function bootstrap() {
       stopAtFirstError: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Tandamos - challenge-nodejs-typescript')
+    .addTag('agents')
+    .addTag('customers')
+    .addTag('orders')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
 
   await app.listen(process.env.PORT);
 }
