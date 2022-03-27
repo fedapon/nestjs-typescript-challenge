ab8dee930bacb147d2a2b8c450e3e8dbdff8b559
fedapon <fedapon@gmail.com>
2022-03-27 11:28:26 -0300

    added swagger documentation to order DTO

--- src/sales/controllers/order/dto/create-order.dto.ts
+++ src/sales/controllers/order/dto/create-order.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsDateString,
   IsNotEmpty,
@@ -8,26 +9,47 @@ import { Agent } from '../../../models/agent.entity';
 import { Customer } from '../../../models/customer.entity';
 
 export class CreateOrderDto {
+  @ApiProperty({
+    example: '200101',
+  })
   @IsNumberString()
   ordNum: number;
 
+  @ApiProperty({
+    example: '3000',
+  })
   @IsNumberString()
   ordAmount: number;
 
+  @ApiProperty({
+    example: '1000',
+  })
   @IsNumberString()
   advanceAmount: number;
 
+  @ApiProperty({
+    example: '2022-03-27',
+  })
   @IsDateString()
   ordDate: Date;
 
+  @ApiProperty({
+    example: 'C00001',
+  })
   @IsNotEmpty()
   @MaxLength(6)
   custCode: Customer;
 
+  @ApiProperty({
+    example: 'A001',
+  })
   @IsNotEmpty()
   @MaxLength(6)
   agentCode: Agent;
 
+  @ApiProperty({
+    example: 'SOD',
+  })
   @IsNotEmpty()
   @MaxLength(60)
   ordDescription: string;
