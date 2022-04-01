f8c62ee920621c1573143de0a7fd3cf266f587ce
Fernando Pons <fedapon@gmail.com>
2022-04-01 00:05:48 -0300

    changed absolute to relative path for testing

--- src/sales/sales.module.ts
+++ src/sales/sales.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Agent } from 'src/sales/models/agent.entity';
-import { Customer } from 'src/sales/models/customer.entity';
-import { Order } from 'src/sales/models/order.entity';
+import { Agent } from '../sales/models/agent.entity';
+import { Customer } from '../sales/models/customer.entity';
+import { Order } from '../sales/models/order.entity';
 import { AgentController } from './controllers/agent/agent.controller';
 import { CustomerController } from './controllers/customer/customer.controller';
 import { OrderController } from './controllers/order/order.controller';
