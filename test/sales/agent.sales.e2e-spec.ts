24e80b2dd6721047c298634b5598396d84b33bc4
Fernando Pons <fedapon@gmail.com>
2022-04-01 23:33:26 -0300

    created basic CRUD e2e tests

--- test/sales/agent.sales.e2e-spec.ts
+++ test/sales/agent.sales.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { SalesModule } from '../../src/sales/sales.module';
 import { getRepositoryToken } from '@nestjs/typeorm';
@@ -10,9 +10,32 @@ import { Order } from '../../src/sales/models/order.entity';
 describe('SalesController (e2e)', () => {
   let app: INestApplication;
 
-  const mockAgentRepository = {
-    find: jest.fn().mockImplementation(() => Promise.resolve({})),
+  const agent = {
+    agentCode: 'A001',
+    agentName: 'Jhon Smith',
+    workingArea: 'London',
+    commission: '0.1',
+    phoneNo: '077-12345674',
+    country: 'USA',
   };
+
+  const mockAgentRepository = {
+    find: jest.fn().mockImplementation(() => Promise.resolve([agent])),
+    create: jest.fn().mockImplementation((dto) => Promise.resolve(dto)),
+    save: jest.fn().mockImplementation((agent) => Promise.resolve(agent)),
+    update: jest
+      .fn()
+      .mockImplementation((agentCode, updateAgentDto) =>
+        Promise.resolve({ agentCode, ...updateAgentDto }),
+      ),
+    delete: jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        raw: [],
+        affected: 1,
+      }),
+    ),
+  };
+
   const mockCustomerRepository = {};
   const mockOrderRepository = {};
 
@@ -29,10 +52,65 @@ describe('SalesController (e2e)', () => {
       .compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        stopAtFirstError: true,
+      }),
+    );
     await app.init();
   });
 
   it('/agents (GET)', () => {
-    return request(app.getHttpServer()).get('/agents').expect(200);
+    return request(app.getHttpServer())
+      .get('/agents')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+      .expect([agent]);
+  });
+
+  it('/agents (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/agents')
+      .send(agent)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+      .expect(agent);
+  });
+
+  it('/agents (POST) should fail because missing parameters', () => {
+    return request(app.getHttpServer())
+      .post('/agents')
+      .send({})
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+  });
+
+  it('/agents (UPDATE)', () => {
+    return request(app.getHttpServer())
+      .patch('/agents/A001')
+      .send({ agentName: 'Jhon Smith' })
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+      .expect({ agentCode: 'A001', agentName: 'Jhon Smith' });
+  });
+
+  it('/agents (UPDATE) should fail because invalid parameter', () => {
+    return request(app.getHttpServer())
+      .patch('/agents/A001')
+      .send({ agentName: 1 })
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+  });
+
+  it('/agents (DELETE)', () => {
+    return request(app.getHttpServer())
+      .delete('/agents/A001')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+      .expect({
+        raw: [],
+        affected: 1,
+      });
   });
 });
