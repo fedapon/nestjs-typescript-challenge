27429d75aeb2b17f3335a2b98ddf9b1a81e3ea35
fedapon <fedapon@gmail.com>
2022-05-08 18:45:04 -0300

    added local strategy

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -6,11 +6,11 @@ import { AuthService } from './../services/auth.service';
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
-    super();
+    super({ usernameField: 'email' });
   }
 
-  async validate(username: string, password: string): Promise<any> {
-    const user = await this.authService.validateUser(username, password);
+  async validate(email: string, password: string): Promise<any> {
+    const user = await this.authService.validateUserLocal(email, password);
     if (!user) {
       throw new UnauthorizedException();
     }
