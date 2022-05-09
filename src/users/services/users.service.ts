ae628f57d4f79f3bd3f543ba95d6ee799e9a9901
fedapon <fedapon@gmail.com>
2022-05-08 21:51:25 -0300

    bcrypt was changed for bcryptjs for docker issues

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../models/user.entity';
 import { CreateUserDto } from '../../auth/controllers/dto/create-user.dto';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
