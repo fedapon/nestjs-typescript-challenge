91523968f1d0e608f5941835ce4455a3654cb9bb
fedapon <fedapon@gmail.com>
2022-05-18 17:19:55 -0300

    changed request from supertest in compliance with ES6 modules spec

--- test/sales/customer.sales.e2e-spec.ts
+++ test/sales/customer.sales.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { SalesModule } from '../../src/sales/sales.module';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Agent } from '../../src/sales/models/agent.entity';
