de0d477a64a7eefb07f765ab9cb53c73973c145c
fedapon <fedapon@gmail.com>
2022-05-18 23:16:36 -0300

    added ConfigModule.forRoot() to the module for timing issues

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,17 +6,22 @@ import { LocalStrategy } from './strategies/local.strategy';
 import { AuthController } from './controllers/auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    UsersModule,
-    PassportModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '24h' },
     }),
+
+    PassportModule,
+    UsersModule,
   ],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
   controllers: [AuthController],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
