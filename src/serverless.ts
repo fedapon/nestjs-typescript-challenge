338c173ad487b865c36af680b8e12560685e7b0b
fedapon <fedapon@gmail.com>
2022-05-22 14:51:54 -0300

    added /dev server to swagger docs

--- src/serverless.ts
+++ src/serverless.ts
@@ -27,6 +27,7 @@ async function bootstrap() {
     .addTag('Customers')
     .addTag('Orders')
     .addBearerAuth()
+    .addServer('/dev')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/api/docs', app, document);
